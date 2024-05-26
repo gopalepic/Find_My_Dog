940a5e3d9bc2235ead1c42f071f11aa6849e8cd9
Gopal soni <Gopals.btech2022@iujaipur.edu.in>
2024-05-26 23:35:12 +0530

    added breed functanility

--- src/Components/Add_your_dog.jsx
+++ src/Components/Add_your_dog.jsx
@@ -2,8 +2,17 @@ import React, { useState } from 'react'
 import {BreedList} from '../Data/BreedList'
 const Add_your_dog = () => {
 
+  const [selectedBreedNAME, setSelectedBreedNAME] = useState("");
   const [searchTerm , setSearchTerm] = useState('');
   const [filteredBreeds , setFilteredBreeds]  = useState([]);
+  const [SelectedBreed , setSelectedBreed] = useState('');
+
+  const selectionOfBreed = (breed) => {
+    selectedBreedNAME = {breed}
+    
+
+   
+  }
 
   const handleSearch = (event) => {
     const userInput = event.target.value.toLowerCase();
@@ -34,15 +43,15 @@ const Add_your_dog = () => {
           <h1 className='text-4xl '> Your dog detials</h1>
           <h1 className='text-2xl'> Name: <input type="text" className=" bg-[#b38b6d] rounded-xl m-2" /> </h1>
           <h1 className='text-2xl'> Age: <input type="number" className=" bg-[#b38b6d] rounded-xl m-2"  /> </h1>
-          <h1 className='text-2xl'> Breed: <div> <input type="text" value={searchTerm} onChange={handleSearch}
-          className=' bg-[#b38b6d] rounded-xl m-2"'/>
+          <h1 className='text-2xl'> Breed:  <input type="text"  onChange={handleSearch}
+          className=' bg-[#b38b6d] rounded-xl m-2 cursor:pointer'/>
              <ul className='bg-[#4B371C] w-1/5 rounded-xl '>
               {filteredBreeds.map((breed , index) => (
-                <li key={index}>{breed}</li>
+                <li key={index} >{<button onClick={() => selectionOfBreed(breed)} className='hover:bg-[#FFE4C4]'>{breed}</button>}</li>
               )
               )}
              </ul>
-          </div> </h1>
+          </h1>
           <h1 className='text-2xl'> Weight: <input type="text" className=" bg-[#b38b6d] rounded-xl m-2" /> </h1>
           <h1 className='text-2xl'> Color: <input type="text" className=" bg-[#b38b6d] rounded-xl m-2" /> </h1>
           <h1 className='text-2xl'> Temperment: <input type="text" className=" bg-[#b38b6d] rounded-xl m-2" /> </h1>
